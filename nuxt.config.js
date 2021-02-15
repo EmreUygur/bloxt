a82288c5c9803f9f3aa7f8bc403a854179b3db86
Emre UYGUR <emre.uygur@outlook.com>
2021-02-15 15:12:04 +0300

    Set global meta information for app

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bloxt',
+    title: 'Bloxt',
+    titleTemplate: '%s | Bloxt',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt + NuxtContent Blog Website Boilerplate',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
