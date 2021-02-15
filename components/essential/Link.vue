1bdb6f9243c8349b30206439558614d64ea5ed0c
Emre UYGUR <emre.uygur@outlook.com>
2021-02-15 15:10:46 +0300

    Initialize Link component

---
+++ components/essential/Link.vue
@@ -0,0 +1,33 @@
+<template>
+  <nuxt-link class="link" :class="variant" :to="to">
+    {{ label }}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.link {
+  color: #000;
+  text-decoration: none;
+  outline: none;
+}
+</style>
