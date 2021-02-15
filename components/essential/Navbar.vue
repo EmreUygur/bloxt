da347c7202ccf51d9fd01898c36e001999acea1d
Emre UYGUR <emre.uygur@outlook.com>
2021-02-15 15:34:58 +0300

    Set links to the navbar

--- components/essential/Navbar.vue
+++ components/essential/Navbar.vue
@@ -1,9 +1,23 @@
 <template>
-  <header class="navbar"></header>
+  <header class="navbar">
+    <div class="navbar-section">
+      <Link to="/" label="Bloxt" />
+    </div>
+    <div class="navbar-section">
+      <Link to="/blog" label="Blog" />
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
 .navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 12px 24px;
+
+  &-section {
+    display: flex;
+  }
 }
 </style>
