bb6dfdc385fad90f9d8a484a7d628a861e23ea69
Emre UYGUR <emre.uygur@outlook.com>
2021-02-15 15:12:46 +0300

    Improve layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <Navbar />
-    <Nuxt />
+    <div class="content-wrapper">
+      <Nuxt />
+    </div>
     <Footer />
   </div>
 </template>
@@ -10,10 +12,15 @@
 body {
   margin: 0;
   .container {
-    width: 100%;
+    width: 1600px;
+    margin: 0 auto;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+    .content-wrapper {
+      width: 1240px;
+      margin: 20px auto;
+    }
   }
 }
 </style>
